99a7ae73714a80f8d924287da9d7cf111b6d8373
Olga Pechenkina <Olha.pechyonkina@yandex.ru>
2024-01-11 22:53:12 +0500

    Initial commit

---
+++ src/test/java/ru/netology/services/FreelancerServiceTest.java
@@ -0,0 +1,45 @@
+package ru.netology.services;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+
+class FreelancerServiceTest {
+
+    @Test
+    public void testcalculateCase1() {
+        FreelancerService service = new FreelancerService();
+
+        int actual = service.calculate(10_000, 3_000, 20_000);
+        int expected = 3;
+
+        Assertions.assertEquals(expected, actual);
+
+
+    }
+
+    @Test
+    public void testcalculateCase2() {
+        FreelancerService service = new FreelancerService();
+
+        int actual = service.calculate(100_000, 60_000, 150_000);
+        int expected = 2;
+
+        Assertions.assertEquals(expected, actual);
+
+
+    }
+
+
+    @ParameterizedTest
+    @CsvFileSource(resources = "test_data.csv")
+    public void testWithParams(int income, int expenses, int threshold, int expected) {
+        FreelancerService service = new FreelancerService();
+
+        int actual = service.calculate(income, expenses, threshold);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    }
